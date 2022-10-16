9f5e4cc170e4fab22725b417e61d9222c04f7725
Jonathan Rojas <jonathan04rojas1990@gmail.com>
2022-10-16 17:41:22 -0600

    Update - pages/ Actualizar Datos
    MODELO

--- lib/index.dart
+++ lib/index.dart
@@ -1,5 +1,7 @@
 import 'package:calle_de_oro_project/pages/ConfigApp.dart';
 import 'package:calle_de_oro_project/pages/EnableAccount.dart';
+import 'package:calle_de_oro_project/pages/NewAccount.dart';
+import 'package:calle_de_oro_project/pages/NewPassword.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -15,7 +17,7 @@ class _IndexState extends State<Index> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ConfigApp(),
+      body: NewAccount(),
     );
   }
 }
