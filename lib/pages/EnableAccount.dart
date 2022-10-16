7803079d384def1e9f49a0cd578dd7dc882220c5
Jonathan Rojas <jonathan04rojas1990@gmail.com>
2022-10-16 16:46:22 -0600

    Update - page/Estado cuenta
    Modelo

--- lib/pages/EnableAccount.dart
+++ lib/pages/EnableAccount.dart
@@ -2,6 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_switch/flutter_switch.dart';
+
+import '../util/util.dart';
 
 class EnableAccount extends StatefulWidget {
   const EnableAccount({Key? key}) : super(key: key);
@@ -11,79 +14,93 @@ class EnableAccount extends StatefulWidget {
 }
 
 class _EnableAccountState extends State<EnableAccount> {
+  static String titulo = "Deshabilitar cuenta";
+  static String subTitulo = "Estado";
+  bool isEstadoCuenta = true;
+  String leyendaEstadoCuenta = "Activa";
   TextEditingController nameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Container(
       // decoration: borderBlack(),
-      child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: ListView(
-            children: <Widget>[
-              Container(
-                  alignment: Alignment.center,
-                  padding: const EdgeInsets.all(10),
-                  child: const Text(
-                    'Sign in',
-                    style: TextStyle(fontSize: 20),
-                  )),
-              Container(
-                padding: const EdgeInsets.all(10),
-                child: TextField(
-                  controller: nameController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'User Name',
-                  ),
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-                child: TextField(
-                  obscureText: true,
-                  controller: passwordController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                  ),
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  //forgot password screen
-                },
-                child: const Text(
-                  'Forgot Password',
-                ),
-              ),
-              Container(
-                  height: 50,
-                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                  child: ElevatedButton(
-                    child: const Text('Login'),
-                    onPressed: () {
-                      print(nameController.text);
-                      print(passwordController.text);
+      margin: const EdgeInsets.all(0),
+      padding: const EdgeInsets.all(0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            margin: const EdgeInsets.fromLTRB(15, 30, 15, 20),
+            // decoration: borderBlack(),
+            child: Text(
+              titulo,
+              style: TextStyle(fontSize: 24),
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            margin: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+            child: Text(
+              subTitulo,
+              style: TextStyle(fontSize: 12),
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            margin: const EdgeInsets.fromLTRB(15, 0, 15, 5),
+            decoration: borderSeparator(),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            // decoration: borderBlack(),
+            // child: contenidoListView(),
+          ),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                    margin: const EdgeInsets.all(0),
+                    padding: const EdgeInsets.all(0),
+                    width: MediaQuery.of(context).size.width / 2.1,
+                    // decoration: borderBlack(),
+                    child: Text(leyendaEstadoCuenta,
+                        style: TextStyle(fontSize: 16))),
+                Container(
+                  margin: const EdgeInsets.all(0),
+                  padding: const EdgeInsets.all(0),
+                  alignment: Alignment.centerRight,
+                  width: MediaQuery.of(context).size.width / 5,
+                  // decoration: borderBlack(),
+                  // child: Text("d"),
+                  child: FlutterSwitch(
+                    width: 50.0,
+                    height: 22.0,
+                    // valueFontSize: 25.0,
+                    // toggleSize: 45.0,
+                    value: isEstadoCuenta,
+                    // borderRadius: 30.0,
+                    // padding: 8.0,
+                    // showOnOff: true,
+                    onToggle: (val) {
+                      setState(() {
+                        isEstadoCuenta = val;
+                        if (isEstadoCuenta) {
+                          leyendaEstadoCuenta = "Activa";
+                        } else {
+                          leyendaEstadoCuenta = "Inactiva";
+                        }
+                      });
                     },
-                  )),
-              Row(
-                children: <Widget>[
-                  const Text('Does not have account?'),
-                  TextButton(
-                    child: const Text(
-                      'Sign in',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    onPressed: () {
-                      //signup screen
-                    },
-                  )
-                ],
-                mainAxisAlignment: MainAxisAlignment.center,
-              ),
-            ],
-          )),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
+    ;
   }
 }
