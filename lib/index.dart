eeea7c67c48caac9e5069fa3caedc998663fef48
Brian Ventura <briandagoberto20@hotmail.com>
2022-10-18 11:05:44 -0600

    Login by Brian Ventura

--- lib/index.dart
+++ lib/index.dart
@@ -2,6 +2,8 @@ import 'package:calle_de_oro_project/pages/ConfigApp.dart';
 import 'package:calle_de_oro_project/pages/EnableAccount.dart';
 import 'package:calle_de_oro_project/pages/NewAccount.dart';
 import 'package:calle_de_oro_project/pages/NewPassword.dart';
+import 'package:calle_de_oro_project/pages/ResetPass.dart';
+import 'package:calle_de_oro_project/pages/login.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -17,7 +19,7 @@ class _IndexState extends State<Index> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: NewAccount(),
+      body: ResetPass(),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: '',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Calle de oro'),
     );
   }
 }
